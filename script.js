c8694ee0b27d5b1f5e3cdc1eef31d2458a428fff
mtrueman16 <32368945+mtrueman16@users.noreply.github.com>
2017-11-07 14:42:12 +0000

    Added cycle plugin and used it for a slideshow

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 $(document).ready(function(){
-var button = $("click_me");
-button.on('click', function () {
-	button.html("Clicked!");
-}
-}
+	var button = $("click_me");
+	button.on('click', function () {
+		button.html("Clicked!");
+	})
+})
+
+
+
+
+//Navbar script - doesn't work
+
+$(document).ready(function(){
+  var square=$(".box")
+  square.on('hover', function(){
+	square.css("border-color", "white");
+
+	})
+})
+
+// End Navbar script
+
+
+$(document).ready(function() {
+	
+    $('.SlideContainer').cycle({
+		fx: 'fade',
+	pause: 1,
+	prev: '#prev',
+	next: '#next'
+
+	});
+});
